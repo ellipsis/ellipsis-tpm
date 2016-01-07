182be437b04dda537d831893580f6505d232c195
G. Roggemans <g.roggemans@grog.be>
2016-01-07 02:20:53 +0100

    Added ellipsis.sh script for ellipsis installation

---
+++ ellipsis.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+##############################################################################
+# @file ellipsis.sh
+# @date January, 2016
+# @author G. Roggemans <g.roggemans@grog.be>
+# @copyright Copyright (c) GROG [https://grog.be] 2016, All Rights Reserved
+# @license MIT
+##############################################################################
+
+pkg.link() {
+    # Link ellipsis-tpm in the Ellipsis bin folder
+    ln -s bin/ellipsis-tpm ${PKG_PATH}../../bin/ellipsis-tpm
+}
+
+##############################################################################
+
+pkg.unlink() {
+    # Remove ellipsis-tpm link from the Ellipsis bin folder
+    rm ${PKG_PATH}../../bin/ellipsis-tpm
+}
+
+##############################################################################
