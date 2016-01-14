e970a86a41c13fcee1ad78ceb4c8122e71fab3b3
G. Roggemans <g.roggemans@grog.be>
2016-01-14 17:22:00 +0100

    Added and implemented src/version.bash

---
+++ src/version.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+##############################################################################
+# @file version.bash
+# @date January, 2016
+# @author G. Roggemans <g.roggemans@grog.be>
+# @copyright Copyright (c) GROG [https://grog.be] 2016, All Rights Reserved
+# @license MIT
+##############################################################################
+
+ELLIPSIS_TPM_VERSION=0.0.1-Alpha
+
+##############################################################################
