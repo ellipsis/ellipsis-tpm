c31e5ace03ae89f49bb30ddbeec0b57ade75a795
G. Roggemans <g.roggemans@grog.be>
2016-02-10 13:13:54 +0100

    Cleaner paths in link/unlink hooks

--- ellipsis.sh
+++ ellipsis.sh
@@ -9,14 +9,14 @@
 
 pkg.link() {
     # Link ellipsis-tpm in the Ellipsis bin folder
-    fs.link_file ${PKG_PATH}/bin/ellipsis-tpm ${PKG_PATH}/../../bin/ellipsis-tpm
+    fs.link_file ${PKG_PATH}/bin/ellipsis-tpm ${ELLIPSIS_PATH}/bin/ellipsis-tpm
 }
 
 ##############################################################################
 
 pkg.unlink() {
     # Remove ellipsis-tpm link from the Ellipsis bin folder
-    rm ${PKG_PATH}/../../bin/ellipsis-tpm
+    rm ${ELLIPSIS_PATH}/bin/ellipsis-tpm
 }
 
 ##############################################################################
